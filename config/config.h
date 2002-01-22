5519fedd7f721d884b4d4298d7de8020721f8511
allen <allen@07b56ee9-b158-4c8b-9baa-ff89d65b121f>
2002-01-22 23:46:37 +0000

    Updated to new config directory structure.
    git-svn-id: https://svn.code.sf.net/p/libemf/code/trunk@17 07b56ee9-b158-4c8b-9baa-ff89d65b121f

---
+++ config/config.h
@@ -0,0 +1,84 @@
+/* config/config.h.  Generated automatically by configure.  */
+/* config/config.h.in.  Generated automatically from configure.ac by autoheader.  */
+
+/* Enable the EditEnhMetaFile() function */
+/* #undef ENABLE_EDITING */
+
+/* Define if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define if you have the <errno.h> header file. */
+#define HAVE_ERRNO_H 1
+
+/* Define if you have the `floor' function. */
+#define HAVE_FLOOR 1
+
+/* Define if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Define if you have the <limits.h> header file. */
+#define HAVE_LIMITS_H 1
+
+/* Define if your system has a working `malloc' function. */
+#define HAVE_MALLOC 1
+
+/* Define if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define if you have the `memset' function. */
+#define HAVE_MEMSET 1
+
+/* Define if you have the <pthread.h> header file. */
+#define HAVE_PTHREAD_H 1
+
+/* Define if you have the `select' function. */
+#define HAVE_SELECT 1
+
+/* Define if you have the <stddef.h> header file. */
+#define HAVE_STDDEF_H 1
+
+/* Define if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Turn on any addition debugging edits */
+/* #undef LIBEMF_DEBUG */
+
+/* Name of package */
+#define PACKAGE "libEMF"
+
+/* Define if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.6"
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define as `__inline' if that's what the C compiler calls it, or to nothing
+   if it is not supported. */
+/* #undef inline */
+
+/* Define to `unsigned' if <sys/types.h> does not define. */
+/* #undef size_t */
